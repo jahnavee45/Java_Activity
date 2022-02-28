e3d88e8f126984498b5dd351c049b79ae5097bcb
jahnavee45 <80255059+jahnavee45@users.noreply.github.com>
2022-02-28 16:35:02 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+public class Details
+{
+  private int run;
+  private String name;
+  private String inning;
+
+  public Details(String name, String inning, int run)
+ {
+   this.name = name;
+   this.inning = inning;
+   this.run = run;
+ }
+
+  public String getName()
+  {
+    return name; 
+  }
+  public String getInning()
+  {
+    return inning;
+  }
+  public int getRuns()
+  {
+    return run;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public void setInning(String inning)
+  {
+    this.inning = inning;
+  }
+
+  public void setRuns(int run)
+  {
+    this.run = run;
+  }
+
+}
+  
